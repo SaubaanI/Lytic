86db32cc556e67d91e3aab096af6107e97ffb127
HaseebSiddiqi <Haseebs727@gmail.com>
2026-03-07 04:06:46 -0500

    Finalized video uploading

--- Backend/backend_1.py
+++ Backend/backend_1.py
@@ -5,7 +5,7 @@ import shutil
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
